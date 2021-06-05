0e1345cf6266da8785fa7d30be59796fc7e9604f
Lucas Didur <ldidur@icloud.com>
2021-06-05 14:11:37 -0300

    refact: rename app

--- packages/mobile/ios/MonorepoAppTests/MonorepoAppTests.m
+++ packages/mobile/ios/MonorepoAppTests/MonorepoAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myprojectnameTests : XCTestCase
+@interface MonorepoAppTests : XCTestCase
 
 @end
 
-@implementation myprojectnameTests
+@implementation MonorepoAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- packages/web-cra/src/index.tsx
+++ packages/web-cra/src/index.tsx
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native'
 
 import { App } from 'components/src/App'
 
-AppRegistry.registerComponent('myprojectname', () => App)
-AppRegistry.runApplication('myprojectname', {
+AppRegistry.registerComponent('monorepo-app', () => App)
+AppRegistry.runApplication('monorepo-app', {
   rootTag: document.getElementById('root'),
 })
